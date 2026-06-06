0b5ebc2bed16ff752467269b60a3800c394b2054
Gözde Kösa <gzd_508@hotmail.com>
2026-06-06 14:07:07 +0300

    my portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       <body className={`${inter.className} min-h-full flex`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
